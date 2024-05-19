2f786da19a7187f8837a0cb9a26a308902af1da2
Maksym <makssshvets@gmail.com>
2024-05-19 22:10:29 +0200

    Formularze + lekko zmieniony interfejs z odwołaniami

--- app/migrations/Version20240519132230.php
+++ app/migrations/Version20240519132230.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240518163509 extends AbstractMigration
+final class Version20240519132230 extends AbstractMigration
 {
     public function getDescription(): string
     {
